166bafdbc0df3b43c9a9fdc0f3f7c005b6ecc1a5
Rafael Augusto <62909878+outflux1998@users.noreply.github.com>
2022-09-22 22:42:22 -0300

    Update App.java

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -2,6 +2,16 @@ public class App {
 
       
     public static void main(String[] args) throws Exception {
+       Grafo grafo = new Grafo("grafo 1");
+       
+       grafo.addVertice(1);
+       grafo.addVertice(2);
+       grafo.addVertice(3);
+       
+       grafo.addAresta(1, 2);
+       grafo.addAresta(2, 3);
+       
+       System.out.println(grafo.existeCaminho(1, 3));
        
     }
 }
