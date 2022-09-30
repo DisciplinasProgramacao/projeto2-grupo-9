1f47f7410e963422fc82a38bef374cfa93989fcb
Rafael Augusto <62909878+outflux1998@users.noreply.github.com>
2022-09-29 21:20:44 -0300

    Update App.java

--- codigo/src/App.java
+++ codigo/src/App.java
@@ -2,16 +2,13 @@ public class App {
 
       
     public static void main(String[] args) throws Exception {
-       Grafo grafo = new Grafo("grafo 1");
        
-       grafo.addVertice(1);
-       grafo.addVertice(2);
-       grafo.addVertice(3);
+            
+       final String path = "C:\\Users\\Rafael\\Documents\\GitHub\\projeto2-grupo-9\\codigo\\grafo.txt";
        
-       grafo.addAresta(1, 2, 0);
-       grafo.addAresta(2, 3, 0);
-       
-       System.out.println(grafo.existeCaminho(1, 3));
+       Grafo grafo = new Grafo("1");
+
+       boolean carregamento = grafo.carregar(path);
        
     }
 }
